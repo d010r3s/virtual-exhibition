f96cc5345691e3b11d9845d637b7696d216e760a
d010r3s <104917935+d010r3s@users.noreply.github.com>
2024-12-15 03:44:15 +0300

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -270,3 +270,28 @@ AFTER INSERT OR UPDATE OR DELETE ON Users
 FOR EACH ROW
 EXECUTE FUNCTION update_participant_count();
 
+CREATE OR REPLACE FUNCTION clean_registered_events()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Оставляем только существующие EventID
+    NEW.RegisteredEvents := array_to_string(
+        ARRAY(
+            SELECT event_id::TEXT
+            FROM unnest(string_to_array(NEW.RegisteredEvents, ',')) AS event_id
+            WHERE EXISTS (
+                SELECT 1
+                FROM Events
+                WHERE Events.EventID = event_id::INTEGER
+            )
+        ), ','
+    );
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_clean_registered_events
+BEFORE INSERT OR UPDATE ON Users
+FOR EACH ROW
+EXECUTE FUNCTION clean_registered_events();
+
